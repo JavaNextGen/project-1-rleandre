c5d5909afe66410b6d5f55b00e33764e5052d8f1
Reginald Leandre <reginald.leandre@revature.com>
2022-01-05 15:31:38 -0600

    Established connection

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,19 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.util.ConnectionFactory;
+
 public class Driver {
 
     public static void main(String[] args) {
+    	
+    	try(Connection conn = ConnectionFactory.getConnection()){
+			System.out.println("Connection Successful :)");
+		} catch(SQLException e) {
+			System.out.println("Connection failed");
+			e.printStackTrace();
+		}
     }
 }
--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -1,6 +1,8 @@
 package com.revature.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * <p>This ConnectionFactory class follows the Singleton Design Pattern and facilitates obtaining a connection to a Database for the ERS application.</p>
@@ -27,12 +29,40 @@ public class ConnectionFactory {
 
         return instance;
     }
-
+    
     /**
      * <p>The {@link ConnectionFactory#getConnection()} method is responsible for leveraging a specific Database Driver to obtain an instance of the {@link java.sql.Connection} interface.</p>
      * <p>Typically, this is accomplished via the use of the {@link java.sql.DriverManager} class.</p>
      */
-    public Connection getConnection() {
-        return null;
+    
+    
+    	public static Connection getConnection() throws SQLException {
+            
+        	//For compatibility with other technologies, we need to register our PostgreSQL Driver
+        	//This process makes the application aware of what database Driver (SQL dialect) we're using
+        	
+        	
+        	try {
+        		Class.forName("org.postgresql.Driver"); //try to find and return the psotgresql Driver Class
+        	} catch (ClassNotFoundException e) {
+        		System.out.println("CLASS WASN'T FOUND");
+        		e.printStackTrace(); //this will print the exception message to the console
+        	}
+        	
+        	//we need to provide our database credentials
+        	//we'll hardcode them for now, but I'll show a way to hide the credentials in environment variables
+        	
+        	//the url to my database schema
+        	String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=project1";
+        	
+        	//your postgres username (should just be postgres)
+        	String username = "postgres";
+        
+        	//your postgres password (hopefully just "password")
+        	String password = "password"; //don't be like me
+        	
+        	//This is what actually returns our Connection object. (Note how this method has a return type of Connection)
+        	return DriverManager.getConnection(url, username, password);
+        	
     }
 }
